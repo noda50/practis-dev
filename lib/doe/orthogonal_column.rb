c85219b829cc2326e8b2b7e27da8a2e019f946c2
Hiroyasu Matsushima <matsushima@matsushima-centos.aist.go.jp>
2013-09-05 15:33:26 +0900

    orthogonal_column.rb is modified

--- lib/doe/orthogonal_column.rb
+++ lib/doe/orthogonal_column.rb
@@ -6,26 +6,30 @@ class OrthogonalColumn
 	INSIDE = "inside"
 	BOTH_SIDE = "both side"
 
+	attr_reader :parameter_name
+	attr_reader :parameters
 	attr_reader :level
 	attr_reader :digit_num
 	attr_reader :corresponds
 
 
 	# start 2 levels of parameter
-	def initialize(initial_parameters)
+	def initialize(parameter_name, initial_parameters)
+		@parameter_name = parameter_name
+		@parameters = initial_parameters
 		@level = initial_parameters.size
-		initial_parameters.sort!
+		@parameters.sort!
 		@corresponds = {}
 		@digit_num = @level / 2
 		for i in 0...@level do
 		  bit = ("%0" + @digit_num.to_s + "b") % i
-		  @corresponds[bit] = params[i]
+		  @corresponds[bit] = @parameters[i]
 		end
 	end
 
 	# check digit number 
 	def equal_digit_num(old_digit_num) 
-	  old_digit_num = @digit_num
+	  # old_digit_num = @digit_num
 	  @digit_num = sqrt(@level).ceil
 	  if old_digit_num < @digit_num
 	    return true
@@ -44,40 +48,54 @@ class OrthogonalColumn
 	  end
 	end
 
+	def update_level(addtional_levels)
+		@level += addtional_levels
+	end
+
 	# in case of new parameter points, check & alignment parameter in order
-	def check_alignment(add_point_case, parameter)
+	def check_alignment(add_point_case, add_parameters)
 	  case add_point_case
 	  when OUTSIDE_PLUSE
 	    right_digit_of_max = @corresponds.max_by(&:last)[0]
 	    if right_digit_of_max[right_digit_of_max.size - 1] == "1"
-	      parameter.sort!
+	      add_parameters.sort!
 	    else
-	      parameter.sort.reverse!
+	      add_parameters.sort.reverse!
 	    end
 	  when OUTSIDE_MINUS
 	    right_digit_of_min = @corresponds.min_by(&:last)[0]
 	    if right_digit_of_min[right_digit_of_max.size - 1] == "0"
-	      parameter.sort!
+	      add_parameters.sort!
 	    else
-	      parameter.sort.reverse!
+	      add_parameters.sort.reverse!
 	    end
 	  when INSIDE
-	    digit_num_of_left_point = @corresponds.max_by { |item| (item[1] < parameter[0]) ? item[1] : -1}[0]
+	    digit_num_of_left_point = @corresponds.max_by { |item| (item[1] < add_parameters[0]) ? item[1] : -1}[0]
 	    if digit_num_of_left_point[digit_num_of_left_point.size - 1] == "0"
-	      parameter.sort.reverse!
+	      add_parameters.sort.reverse!
 	    else
-	      parameter.sort!
+	      add_parameters.sort!
 	    end
 	  when BOTH_SIDE
 	    right_digit_of_max = @corresponds.max_by(&:last)[0]
 	    if right_digit_of_max[right_digit_of_max.size - 1] == "1"
-	      parameter.sort.reverse!
+	      add_parameters.sort.reverse!
 	    else
-	      parameter.sort!
+	      add_parameters.sort!
 	    end
 	  else
 	    p "error"
 	  end
-	  return parameter
+	  @parameters += add_parameters
+	end
+
+	# 
+	def assign_parameter(old_level, parameters)
+	  for i in old_level...@level do
+	    bit = ("%0" + @digit_num.to_s + "b") % i
+	    if !@corresponds.key?(bit)
+	      @corresponds[bit] = parameters[i]
+	    end
+	  end
 	end
 end
