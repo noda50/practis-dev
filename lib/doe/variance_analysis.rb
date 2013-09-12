f11b4efa07152de18ef7dbd5f994f6cfe6acc6fa
Hiroyasu Matsushima <matsushima@matsushima-centos.aist.go.jp>
2013-09-12 23:14:18 +0900

    modify

--- lib/doe/variance_analysis.rb
+++ lib/doe/variance_analysis.rb
@@ -1,11 +1,11 @@
 class VarianceAnalysis
 
-  attr_reader :effFact_list
+  attr_reader :effect_Factor
+  attr_reader :e_f
   
   # 
   def initialize(result_set, orthogonal_table, colums)
-    # combi_size = 0
-    @effFact_list = []
+    @effect_Factor = []
 
     @m = result_set[:results].values.inject(:+).inject(:+)
     @ss = result_set[:results].values.inject(:+).inject{|ss, n| ss + n**2}
@@ -40,15 +40,15 @@ class VarianceAnalysis
       effFact[:results].each_value{|v| effFact[:effect] += ((v.inject(:+)**2).to_f / v.size)}
       effFact[:effect] -= @ct
       effFact[:free] = 1 #(2水準なので常に1)
-      @effFact_list.push(effFact) #main effect (v)  = :effect / free 
+      @effect_Factor.push(effFact) #main effect (v)  = :effect / free 
     end
 
     # 残差変動
-    @s_e = @ss - (@ct + @effFact_list.inject(0){|sum, ef| sum + ef[:effect]})
+    @s_e = @ss - (@ct + @effect_Factor.inject(0){|sum, ef| sum + ef[:effect]})
     # 残差の自由度
     @e_f = result_set[:area].size - 1 # 実験回数(num of combination) - 平均の自由度
     @e_v  = @s_e / @e_f
-    @effFact_list.each{|fact| fact[:f_value] = fact[:effect] / @e_v}
+    @effect_Factor.each{|fact| fact[:f_value] = fact[:effect] / @e_v}
 
     # p "s_e: " + @s_e.to_s
     # p "e_f: " + @e_f.to_s
